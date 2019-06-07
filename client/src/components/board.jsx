63d497bc3b66079b1267bb32a0a57db1d5babc9a
Jared Ellison <jaredellison@gmail.com>
2019-06-07 09:46:35 -0400

    Brainstorm app state

--- client/src/components/board.jsx
+++ client/src/components/board.jsx
@@ -7,7 +7,26 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      gameState: "newGame", // "new game", "playing", "winner", "game over"
+      sizeX: 9,
+      sizeY: 9,
+      mines: 10,
+      timeCount: 0,
+      flagCount: 0,
+      minesMarked: 0,
+      squareCounter: null, // adjacent mines 0 -> 8
+      squareState: null // "covered", "uncovered", "flagged", "mine"
     };
+  }
+
+  componentDidMount() {
+    this.startNewGame();
+  }
+
+  startNewGame() {
+    // create squareState array based on sizeX and sizeY
+    // fill board with mines based on this.state.mines
+    // calculate square counts based based on mines
   }
 
   render() {
