3dfec7c07ee8dd1308e070663174d97e1f3e4282
Jared Ellison <jaredellison@gmail.com>
2019-06-07 17:47:27 -0400

    Implement styling for uncovered square

--- client/src/components/square.jsx
+++ client/src/components/square.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
 const Square = props => {
-  const status = props.status || "covered";
-
   return (
-    <div className="square" onClick={props.clickHandler}>
+    <div className={"square" + (props.uncovered ? " uncovered" : "")} onClick={props.clickHandler}>
       {props.uncovered && props.count}
     </div>
   );
