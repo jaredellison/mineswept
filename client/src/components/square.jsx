11bac53ec89ac7ebdd2f97e786ccff443de66445
Jared Ellison <jaredellison@gmail.com>
2019-06-07 21:19:08 -0400

    Allow square to display mines

--- client/src/components/square.jsx
+++ client/src/components/square.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Square = props => {
   return (
     <div className={"square" + (props.uncovered ? " uncovered" : "")} onClick={props.clickHandler}>
+      {props.gameState === "game-over" && props.mine && "💣"}
       {props.uncovered && props.count}
     </div>
   );
