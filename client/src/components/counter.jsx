94ce3e1cfb8192809140f7b6494c9fd6a7e14598
Jared Ellison <jaredellison@gmail.com>
2019-06-09 18:26:42 -0400

    Render negative numbers

--- client/src/components/counter.jsx
+++ client/src/components/counter.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Counter = (props) => {
   const count = props.count || 0;
-  const countStr = String(count).padStart(3, "0");
+  let countStr;
+
+  if (count >= 0) {
+    countStr = String(count).padStart(3, '0');
+  } else {
+    countStr = '-' + String(Math.abs(count)).padStart(2, '0');
+  }
+
   return (
   <div className="counter">
     {countStr}
