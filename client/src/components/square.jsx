712109ad0fab61006c1ccca4c18bab140a086ec3
Jared Ellison <jaredellison@gmail.com>
2019-06-23 01:07:10 -0400

    Add color styling to square counts

--- client/src/components/square.jsx
+++ client/src/components/square.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
+const numberStyling = {
+  0: '',
+  1: ' number-1',
+  2: ' number-2',
+  3: ' number-3',
+  4: ' number-4',
+  5: ' number-5',
+  6: ' number-6',
+  7: ' number-7',
+  8: ' number-8'
+};
+
 const Square = props => {
   return (
     <td
       className={
         'square' +
         (props.uncovered && !props.flag ? ' uncovered' : '') +
-        (props.losing ? ' losing' : '')
+        (props.losing ? ' losing' : '') +
+        (props.count !== null ? numberStyling[props.count] : '')
       }
       onMouseDown={props.clickHandler}
     >
